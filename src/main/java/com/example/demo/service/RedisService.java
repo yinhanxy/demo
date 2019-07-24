809816cdeb453fccdfefc810015bf3c1862a525a
yh <yinhanxy@gmail.com>
2019-07-24 23:30:01 +0800

    添加redisService

--- src/main/java/com/example/demo/service/RedisService.java
+++ src/main/java/com/example/demo/service/RedisService.java
@@ -13,4 +13,24 @@ public interface RedisService {
      * @return
      */
     String get(String key);
+
+    /**
+     * 设置过期时间
+     * @param key
+     * @param exprit
+     * @return
+     */
+    boolean expire(String key, long exprit);
+
+    /**
+     * 删除数据
+     */
+    void remove(String key);
+
+    /**
+     * 自增操作
+     * @param delta 自增步长
+     */
+    Long increment(String key, long delta);
+
 }
